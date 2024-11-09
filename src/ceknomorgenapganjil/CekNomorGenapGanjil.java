89cd6313e8eef12882e1b5745d765d573fc32f88
ainunnisaaaa <ainunnisa60268@gmail.com>
2024-11-09 21:58:50 +0800

    Initial commit

---
+++ src/ceknomorgenapganjil/CekNomorGenapGanjil.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ceknomorgenapganjil;
+
+/**
+ *
+ * @author asus
+ */
+public class CekNomorGenapGanjil {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
